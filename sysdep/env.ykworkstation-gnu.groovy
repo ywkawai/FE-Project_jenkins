41cdafa22909d21c52f09fa0ae3cf03e8aa7aad9
Yuta Kawai <yuta.kawai@riken.jp>
2020-12-24 23:08:28 +0900

    Modify files.

---
+++ sysdep/env.ykworkstation-gnu.groovy
@@ -0,0 +1,8 @@
+#!groovy
+
+env.PATH               = "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:"
+env.LD_LIBRARY_PATH    = ""
+
+env.SCALE_FE_SYS        = "Linux64-gnu-ompi"
+env.SCALE               = "/home/ykawai/workspace/scale/"
+env.SCALE_ENABLE_OPENMP = "T"
