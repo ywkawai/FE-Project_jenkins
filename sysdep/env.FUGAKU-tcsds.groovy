1b70bb2e09e02824941c02b15db9c356f3c72f85
Yuta Kawai <yuta.kawai@riken.jp>
2023-06-28 21:13:15 +0900

    Add Jenkinsfile for FUGAKU

---
+++ sysdep/env.FUGAKU-tcsds.groovy
@@ -0,0 +1,9 @@
+#!groovy
+
+env.PATH               = "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:"
+env.LD_LIBRARY_PATH    = ""
+
+env.SCALE_FE_SYS         = "FUGAKU"
+env.SCALE                = "/data/ra000005/a04028/SCALE_comp_perf/"
+env.SCALE_ENABLE_OPENMP  = "T"
+env.SETUP_SPACK_SCALE_SH = "~/setup_spack_scale.sh"
