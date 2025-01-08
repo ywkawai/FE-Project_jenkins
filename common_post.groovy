ce1c901dc451542caf389f2f38ed0f14cbb75b47
Yuta Kawai <yuta.kawai@riken.jp>
2025-01-08 16:54:15 +0900

    Perform rsync

--- common_post.groovy
+++ common_post.groovy
@@ -1,7 +1,10 @@
 def rsync_artifacts(Map params = [:]){
     def target_str = "${params.target_dir}"
     def dist_str = "${env.SCALE_ARTIFACT_DIR}/${env.JOB_NAME}/${env.BUILD_NUMBER}/${params.target_dir}"
-    println(target_str)
-    println(dist_str)
+    // println(target_str)
+    // println(dist_str)
+    println(['rsync', '-tpgv', params.target_dir, dist_str].execute().text)
+
+
 }
 return this
