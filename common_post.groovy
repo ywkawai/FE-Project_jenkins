56f5a033828e9d7666b370be871fa3f2e5d4236e
Yuta Kawai <yuta.kawai@riken.jp>
2025-01-09 15:07:03 +0900

    Modify post processes

--- common_post.groovy
+++ common_post.groovy
@@ -2,17 +2,18 @@ def rsync_sample_artifacts(Map params = [:]){
     def target_str = "${params.target_sample_dir}/${params.sample_name}/"
     def dist_str = "${env.SCALE_ARTIFACT_DIR}/${env.JOB_NAME}/${env.BUILD_NUMBER}/${params.sample_name}/"
     params.copy_src_list.each{ copy_src ->
+        // println(['bash', '-c', "ls -lha ${target_str}/${copy_src}"])
         println("rsync.. ${target_str}/${copy_src} --> ${dist_str}")
-        println(['rsync', '-tpgvr', '--mkpath', "${target_str}/${copy_src}", dist_str].execute().text)
+        println(['bash', '-c', "rsync -tpgvr --mkpath ${target_str}/${copy_src} ${dist_str}"].execute().text)
     }
 }
 
 def extract_run_LOG_info(Map params = [:]){
     def target_log = "${params.target_sample_dir}/${params.sample_name}/${params.log_fname}"
-    def command = '''
-        cat ${target_log} sed -n '/&PARAM/,/\\//p' > ${target_log}_param_conf
-        cat ${target_log} sed -n '/Computational Time Report/,$p > ${target_log}_rapreport'
-    '''
+    def command = """
+        cat ${target_log} | sed -n '/&PARAM/,/\\//p' > ${target_log}_param_conf
+        cat ${target_log} | sed -n '/Computational Time Report/,\$p' > ${target_log}_rapreport
+    """
     def proc = ['bash', '-c', command].execute()
     proc.waitFor()
     println(proc.text)
