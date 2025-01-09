3ee307748c7775deda2a56781f083a858d08f73d
Yuta Kawai <yuta.kawai@riken.jp>
2025-01-09 11:39:02 +0900

    Add escape character

--- common_post.groovy
+++ common_post.groovy
@@ -10,7 +10,7 @@ def rsync_sample_artifacts(Map params = [:]){
 def extract_run_LOG_info(Map params = [:]){
     def target_log = "${params.target_sample_dir}/${params.sample_name}/${params.log_fname}"
     def command = '''
-        cat ${target_log} sed -n '/&PARAM/,/\//p' > ${target_log}_param_conf
+        cat ${target_log} sed -n '/&PARAM/,/\\//p' > ${target_log}_param_conf
         cat ${target_log} sed -n '/Computational Time Report/,$p > ${target_log}_rapreport'
     '''
     def proc = ['bash', '-c', command].execute()
