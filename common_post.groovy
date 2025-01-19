39fed64ff3350765ec7c34f1a2bc28bcd5137984
Yuta Kawai <yuta.kawai@riken.jp>
2025-01-19 16:01:30 +0900

    Modify script to collect LOG info on remote nodes

--- common_post.groovy
+++ common_post.groovy
@@ -1,3 +1,5 @@
+import hudson.util.RemotingDiagnostics
+
 def rsync_sample_artifacts(Map params = [:]){
     def target_str = "${params.target_sample_dir}/${params.sample_name}/"
     def dist_str = "${env.SCALE_ARTIFACT_DIR}/${env.JOB_NAME}/${env.BUILD_NUMBER}/${params.sample_name}/"
@@ -18,4 +20,21 @@ def extract_run_LOG_info(Map params = [:]){
     proc.waitFor()
     println(proc.text)
 }
+
+def extract_run_LOG_info_remote(Map params = [:]){
+    def target_log = "${params.target_sample_dir}/${params.sample_name}/${params.log_fname}"
+    println(target_log)
+    def command = """
+    def p1 = ['bash', '-c', "cat ${target_log} | sed -n '/&PARAM/,/\\\\//p' > ${target_log}_param_conf"].execute()
+    p1.waitFor()
+    println p1.in.text
+    
+    def p2 = ['bash', '-c', "cat ${target_log} | sed -n '/Computational Time Report/,\\\$p' > ${target_log}_rapreport"].execute()
+    p2.waitFor()
+    println p2.in.text
+    """
+    // println(command)
+    println RemotingDiagnostics.executeGroovy(command, Jenkins.instance.slaves.find { it.getLabelString() == params.node_label }.channel)    
+}
+
 return this
