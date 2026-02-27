1f12176873bccf6dc02fbe9a6dd317fdf4679935
Yuta Kawai <yuta.kawai@riken.jp>
2026-02-27 20:47:19 +0900

    Remove export statements

--- sysdep/env.ykworkstation-nvidia.groovy
+++ sysdep/env.ykworkstation-nvidia.groovy
@@ -6,9 +6,6 @@ SCALE_NETCDF_F        = "/home/apps/oss/spack/opt/spack/linux-a64fx/netcdf-fortr
 env.PATH               = "/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/compilers/extras/qd/bin:/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/comm_libs/mpi/bin:/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/compilers/bin:/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/cuda/bin:/home/ykawai/.local/bin:/home/ykawai/.pyenv/shims:/home/ykawai/.pyenv/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:"
 env.LD_LIBRARY_PATH    = "/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/comm_libs/nvshmem/lib:/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/comm_libs/nccl/lib:/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/math_libs/lib64:/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/compilers/lib:/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/compilers/extras/qd/lib:/opt/nvidia/hpc_sdk/Linux_x86_64/26.1/cuda/lib64"
 
-export SCALE_NETCDF_INCLUDE="-I/home/ykawai/lib/netcdf4-nvidia-fortran/include -I/home/ykawai/lib/netcdf4-nvidia-fortran/include"
-export SCALE_NETCDF_LIBS="-L/home/ykawai/lib/netcdf4-nvidia-fortran/lib -lnetcdff -L/usr/lib/x86_64-linux-gnu/hdf5/serial/lib -L/home/ykawai/lib/netcdf4-nvdia/lib -lnetcdf -lnetcdf -lm"
-
 env.SCALE_FE_SYS         = "Linux64-nvidia"
 env.SCALE                = "/home/ykawai/workspace/scale_openacc/"
 env.SCALE_ENABLE_OPENMP  = "F"
